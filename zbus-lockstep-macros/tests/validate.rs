faccc3a94d2df67e325bf846047083d36d7e8668
Luuk van der Duim <luukvanderduim@gmail.com>
2023-08-18 17:28:03 +0200

    ✅

--- zbus-lockstep-macros/tests/validate.rs
+++ zbus-lockstep-macros/tests/validate.rs
@@ -5,11 +5,21 @@ use zbus::zvariant::{OwnedObjectPath, Type};
 use zbus_lockstep_macros::validate;
 
 #[test]
-fn test_macro() {
+fn test_validate_macro_env_variable_set() {
     // set env variable to enable validation
     std::env::set_var("ZBUS_LOCKSTEP_XML_PATH", "zbus-lockstep-macros/tests/xml");
 
     #[validate]
+    #[derive(Debug, Type)]
+    struct AddNodeEvent {
+        _name: String,
+        _path: OwnedObjectPath,
+    }
+}
+
+#[test]
+fn test_validate_macro_t() {
+    #[validate(xml: "zbus-lockstep-macros/tests/xml")]
     #[derive(Debug, Type)]
     struct AddNodeEvent {
         _name: String,
