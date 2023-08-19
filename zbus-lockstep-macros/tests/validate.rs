8bd0820bf033cb6ac4e158f658f99f4aec34feb1
Luuk van der Duim <luukvanderduim@gmail.com>
2023-08-19 13:50:21 +0200

    ✅ [macros] Add tests
    Adds tests for `RemoveNode` with either an xml-path as env-varialbe or as
    argument.
    
    Adds test where with a custom event name.

--- zbus-lockstep-macros/tests/validate.rs
+++ zbus-lockstep-macros/tests/validate.rs
@@ -5,7 +5,7 @@ use zbus::zvariant::{OwnedObjectPath, Type};
 use zbus_lockstep_macros::validate;
 
 #[test]
-fn test_validate_macro_path_as_env_variable() {
+fn test_validate_macro_node_add_path_as_env_variable() {
     // set env variable to enable validation
     std::env::set_var("ZBUS_LOCKSTEP_XML_PATH", "zbus-lockstep-macros/tests/xml");
 
@@ -18,10 +18,46 @@ fn test_validate_macro_path_as_env_variable() {
 }
 
 #[test]
-fn test_validate_macro_path_as_arg() {
+fn test_validate_macro_node_add_path_as_arg() {
     #[validate(xml: "zbus-lockstep-macros/tests/xml")]
     #[derive(Debug, Type)]
     struct AddNodeEvent {
+        _name: String,
+        _path: OwnedObjectPath,
+    }
+}
+
+#[test]
+fn test_validate_macro_path_node_remove_as_env_variable() {
+    // set env variable to enable validation
+    std::env::set_var("ZBUS_LOCKSTEP_XML_PATH", "zbus-lockstep-macros/tests/xml");
+
+    #[validate]
+    #[derive(Debug, Type)]
+    struct RemoveNodeEvent {
+        _name: String,
+        _path: OwnedObjectPath,
+    }
+}
+
+#[test]
+fn test_validate_macro_remove_node_path_as_arg() {
+    #[validate(xml: "zbus-lockstep-macros/tests/xml")]
+    #[derive(Debug, Type)]
+    struct RemoveNodeEvent {
+        _name: String,
+        _path: OwnedObjectPath,
+    }
+}
+
+#[test]
+fn test_validate_macro_path_node_remove_with_custom_name() {
+    // set env variable to enable validation
+    std::env::set_var("ZBUS_LOCKSTEP_XML_PATH", "zbus-lockstep-macros/tests/xml");
+
+    #[validate(signal: "RemoveNode")]
+    #[derive(Debug, Type)]
+    struct DeletionEvent {
         _name: String,
         _path: OwnedObjectPath,
     }
